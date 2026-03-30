66e468f80c90ef568b2e78be2b2ee3121952f7e7
ElFazaa <hasbialfaza@gmail.com>
2026-03-30 23:13:18 +0700

    Main

--- main.c
+++ main.c
@@ -1,59 +1,89 @@
 #include <stdio.h>
-#include "editor.h"
+#include <string.h>
+#include "file_manager.h"
+#include "text_editor.h"
+#include "text_tools.h"
+
+#define MAX 1000
 
 int main() {
-    Editor editor;
-    initEditor(&editor);
-
-    int choice;
-    char line[256];
+    char buffer[MAX] = "";
+    char clipboard[MAX] = "";
     char filename[100];
+    int choice;
 
-    while (1) {
-        printf("\n===== CLI TEXT EDITOR =====\n");
-        printf("1. Tampilkan teks\n");
-        printf("2. Tambah baris\n");
-        printf("3. Hapus baris\n");
-        printf("4. Simpan file\n");
-        printf("5. Buka file\n");
-        printf("0. Keluar\n");
-        printf("Pilih: ");
+    do {
+        printf("\nTEKS EDITOR THEMIDZZ\n");
+        printf("ISI SEKARANG:\n%s\n", buffer);
+
+        printf("\nMenu:\n");
+        printf("1. New File\n");
+        printf("2. Open File\n");
+        printf("3. Save File\n");
+        printf("4. Delete File\n");
+        printf("5. Tambah Teks\n");
+        printf("6. Copy\n");
+        printf("7. Paste\n");
+        printf("8. Cut\n");
+        printf("9. Find & Replace\n");
+        printf("10. Close Tab\n");
+        printf("0. Exit\n");
+        printf("Pilihan: ");
         scanf("%d", &choice);
         getchar();
 
-        switch(choice) {
+        switch (choice) {
+            case 1:
+                newFile(buffer);
+                break;
 
-        case 1:
-            display(&editor);
-            break;
+            case 2:
+                printf("Nama file: ");
+                scanf("%s", filename);
+                getchar();
+                openFile(filename, buffer);
+                break;
 
-        case 2:
-            printf("Masukkan teks: ");
-            fgets(line, sizeof(line), stdin);
-            insertLine(&editor, line);
-            break;
+            case 3:
+                printf("Nama file: ");
+                scanf("%s", filename);
+                getchar();
+                saveFile(filename, buffer);
+                break;
 
-        case 3:
-            int n;
-            printf("Hapus baris ke: ");
-            scanf("%d",&n);
-            deleteLine(&editor,n-1);
-            break;
+            case 4:
+                printf("Nama file: ");
+                scanf("%s", filename);
+                getchar();
+                deleteFile(filename);
+                break;
 
-        case 4:
-            printf("Nama file: ");
-            scanf("%s",filename);
-            saveFile(&editor,filename);
-            break;
+            case 5:
+                appendText(buffer);
+                break;
 
-        case 5:
-            printf("Nama file: ");
-            scanf("%s",filename);
-            openFile(&editor,filename);
-            break;
+            case 6:
+                copyText(buffer, clipboard);
+                break;
 
-        case 0:
-            return 0;
+            case 7:
+                pasteText(buffer, clipboard);
+                break;
+
+            case 8:
+                cutText(buffer, clipboard);
+                break;
+
+            case 9:
+                findReplace(buffer);
+                break;
+
+            case 10:
+                closeTab();
+                break;
         }
-    }
+
+    } while (choice != 0);
+
+    return 0;
 }
