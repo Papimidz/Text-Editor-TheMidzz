b43b6608661cfd23681ea217715fc56273c458b0
ElFazaa <hasbialfaza@gmail.com>
2026-03-30 23:44:56 +0700

    FAZA BERSABDA PASTE CUT COPY DONE

---
+++ text_editor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "text_editor.h"
+
+void copyText(char *buffer, char *clipboard) {
+    strcpy(clipboard, buffer);
+    printf("[TEXT EDITOR] Copy berhasil\n");
+}
+
+void pasteText(char *buffer, char *clipboard) {
+    strcat(buffer, clipboard);
+    printf("[TEXT EDITOR] Paste berhasil\n");
+}
+
+void cutText(char *buffer, char *clipboard) {
+    strcpy(clipboard, buffer);
+    buffer[0] = '\0';
+    printf("[TEXT EDITOR] Cut berhasil\n");
+}
---
+++ text_editor.h
@@ -0,0 +1,8 @@
+#ifndef TEXT_EDITOR_H
+#define TEXT_EDITOR_H
+
+void copyText(char *buffer, char *clipboard);
+void pasteText(char *buffer, char *clipboard);
+void cutText(char *buffer, char *clipboard);
+
+#endif
