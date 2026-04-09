015e3be00f906ac047e31f2cee4776f22724370f
ElFazaa <hasbialfaza@gmail.com>
2026-04-09 20:17:26 +0700

    update

--- file_manager.c
+++ file_manager.c
@@ -2,6 +2,38 @@
 #include <string.h>
 #include "file_manager.h"
 
+
+void saveFile(const char *filename, char buffer[][100], int lineCount) {
+    FILE *fp = fopen(filename, "w");
+    if (!fp) {
+        printf("Gagal save file\n");
+        return;
+    }
+
+    for (int i = 0; i < lineCount; i++) {
+        fprintf(fp, "%s\n", buffer[i]);
+    }
+
+    fclose(fp);
+    printf("File disimpan\n");
+}
+
+void openFile(const char *filename, char buffer[][100], int *lineCount) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        printf("File tidak ditemukan\n");
+        return;
+    }
+
+    *lineCount = 0;
+
+    while (fgets(buffer[*lineCount], 100, fp)) {
+        buffer[*lineCount][strcspn(buffer[*lineCount], "\n")] = 0;
+        (*lineCount)++;
+    }
+
+    fclose(fp);
+    printf("File dibuka\n");
 void saveFile(const char *filename, char *buffer) {
     FILE *fp = fopen(filename, "w");
     if (!fp) {
@@ -33,6 +65,9 @@ void openFile(const char *filename, char *buffer) {
 
 void deleteFile(const char *filename) {
     if (remove(filename) == 0)
+        printf("File dihapus permanen\n");
+    else
+        printf("Gagal hapus file\n");
         printf("[FILE] File dihapus\n");
     else
         printf("[FILE] Gagal hapus\n");
--- text_tools.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEXT_TOOLS_H
-#define TEXT_TOOLS_H
-
-void newFile(char *buffer);
-void appendText(char *buffer);
-void findReplace(char *buffer);
-void closeTab();
-
-#endif
