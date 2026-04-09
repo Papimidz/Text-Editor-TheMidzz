5099eace4eb7971b5168b206fdf8cefe1a420952
chyntia0630 <chyntiaaulia960@gmail.com>
2026-04-09 18:45:15 +0700

    feat: update file_manager

---
+++ file_manager.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "file_manager.h"
+
+void saveFile(const char *filename, char buffer[][100], int lineCount) {
+    FILE *fp = fopen(filename, "w");
+    if (!fp) {
+        printf("Gagal save file\n");
+        return;
+    }
+
+    for (int i = 0; i < lineCount; i++) {
+        fprintf(fp, "%s\n", buffer[i]);
+    }
+
+    fclose(fp);
+    printf("File disimpan\n");
+}
+
+void openFile(const char *filename, char buffer[][100], int *lineCount) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        printf("File tidak ditemukan\n");
+        return;
+    }
+
+    *lineCount = 0;
+
+    while (fgets(buffer[*lineCount], 100, fp)) {
+        buffer[*lineCount][strcspn(buffer[*lineCount], "\n")] = 0;
+        (*lineCount)++;
+    }
+
+    fclose(fp);
+    printf("File dibuka\n");
+}
+
+void deleteFile(const char *filename) {
+    if (remove(filename) == 0)
+        printf("File dihapus permanen\n");
+    else
+        printf("Gagal hapus file\n");
+}
