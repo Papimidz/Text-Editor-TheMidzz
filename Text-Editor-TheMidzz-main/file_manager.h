2239a49e2de922c017ffcbed0e58d49128d468f2
Papimidz <ziahamidazizi@gmail.com>
2026-04-09 15:20:38 +0700

    Delete Text-Editor-TheMidzz-main directory

--- Text-Editor-TheMidzz-main/file_manager.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILE_MANAGER_H
-#define FILE_MANAGER_H
-
-void saveFile(const char *filename, char *buffer);
-void openFile(const char *filename, char *buffer);
-void deleteFile(const char *filename);
-
-#endif
