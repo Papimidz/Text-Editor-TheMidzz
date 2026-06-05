7c8ee5e4da53a83b1ed846abd97fba00c3b8efaa
chyntia0630 <chyntiaaulia960@gmail.com>
2026-06-05 22:02:13 +0700

    edit lagi

--- chyntia.c
+++ chyntia.c
@@ -50,3 +50,43 @@ void saveFile(EditorState *state) {
     fclose(file);
     printf("\n>> File \"%s\" berhasil disimpan.\n", state->filename);
 }
+
+
+
+void openFile(char *filename, EditorState *state) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("\n>> File \"%s\" tidak ditemukan. Gagal membuka file.\n", filename);
+        return;
+    }
+
+    LineNode *P = state->head;
+    LineNode *hapus;
+    while (P != NULL){
+        hapus = P;
+        P = P->next;
+        clearText(&(hapus->lineContent));
+        free(hapus);
+    }
+    CreateEmpty(state);
+
+    strcpy(state->filename, filename);
+
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        buffer[strcspn(buffer, "\n")] = 0;
+        
+        addLine(state, buffer);
+    }
+
+    fclose(file);
+    printf("\n>> File \"%s\" berhasil dimuat ke editor.\n", filename);
+}
+
+void deleteFile(char *filename) {
+    if (remove(filename) == 0) { 
+        printf("\n>> File \"%s\" berhasil dihapus dari sistem.\n", filename);
+    } else {
+        printf("\n>> Gagal menghapus file. Periksa kembali nama file Anda.\n");
+    }
+}
